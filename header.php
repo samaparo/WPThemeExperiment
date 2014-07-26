7e889d2a428617a216d428745abc09fe8575e98d
Sam <mirthquakes@gmail.com>
2014-07-26 16:29:01 -0500

    move master content into header and footer php files

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title><?php bloginfo("name"); ?></title>
+		<link href="<?php bloginfo("template_directory"); ?>/css/base.css" rel="stylesheet" type="text/css" />
+		<link href="<?php bloginfo("template_directory"); ?>/css/skeleton.css" rel="stylesheet" type="text/css" />
+		<link href="<?php bloginfo("template_directory"); ?>/css/layout.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<header>
+			
+		</header>
+		<section id="mainContent">
